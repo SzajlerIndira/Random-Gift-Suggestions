c45b4ab473422962670df16615d4258b1b66a72a
doraKoreny <dorakoreny@gmail.com>
2019-04-02 22:20:43 +0200

    index page is working, after filling the form a person entity is sent by AppController

--- random-gifter/src/main/java/com/codecool/randomgifter/controller/AppController.java
+++ random-gifter/src/main/java/com/codecool/randomgifter/controller/AppController.java
@@ -1,9 +1,11 @@
 package com.codecool.randomgifter.controller;
 
+import com.codecool.randomgifter.model.Person;
 import com.codecool.randomgifter.model.User;
 import com.codecool.randomgifter.service.DataSaverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -18,13 +20,20 @@ public class AppController {
         return new User();
     }
 
+    @ModelAttribute("person")
+    public Person getPerson() {
+        return new Person();
+    }
+
     @GetMapping(value = "/")
-    public String welcomeView(@ModelAttribute User user) {
+    public String welcomeView(Model model) {
+        model.addAttribute("person", getPerson());
         return "index";
     }
 
     @PostMapping(value = "/addnewperson")
-    public void addNewPerson(@RequestBody String person) {
+    public void addNewPerson(@ModelAttribute Person person) {
+        System.out.println("From appController " + person);
         dataSaverService.createPerson(person);
     }
 
--- random-gifter/src/main/java/com/codecool/randomgifter/service/DataSaverService.java
+++ random-gifter/src/main/java/com/codecool/randomgifter/service/DataSaverService.java
@@ -1,8 +1,12 @@
 package com.codecool.randomgifter.service;
 
+import com.codecool.randomgifter.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,12 +16,17 @@ public class DataSaverService {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Value("data-server.url")
+    @Value("${data-saver.url}")
     private String dataSaverUrl;
 
-    public void createPerson(String person) {
-        HttpEntity<String> request = new HttpEntity<>(person);
-        restTemplate.postForEntity(dataSaverUrl, request, String.class);
+    public String createPerson(Person person) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        HttpEntity<Person> request = new HttpEntity<>(person);
+        System.out.println("Coming from DataSaverService");
+        ResponseEntity<String> response = restTemplate.postForEntity(dataSaverUrl, request, String.class);
+        System.out.println(response);
+        return "SUCCESS";
     }
 
 
