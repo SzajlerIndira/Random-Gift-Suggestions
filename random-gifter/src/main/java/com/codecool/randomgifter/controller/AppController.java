4187c50e313cffd15b2b107a1e21844bf564a5aa
doraKoreny <dorakoreny@gmail.com>
2019-04-02 15:47:46 +0200

    Added RestTemplate to RandomGifter, form to index.html, created DataSaverService

--- random-gifter/src/main/java/com/codecool/randomgifter/controller/AppController.java
+++ random-gifter/src/main/java/com/codecool/randomgifter/controller/AppController.java
@@ -1,14 +1,17 @@
 package com.codecool.randomgifter.controller;
 
 import com.codecool.randomgifter.model.User;
+import com.codecool.randomgifter.service.DataSaverService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @SessionAttributes({"user"})
 public class AppController {
+
+    @Autowired
+    private DataSaverService dataSaverService;
 
     @ModelAttribute("user")
     public User getUser() {
@@ -18,6 +21,11 @@ public class AppController {
     @GetMapping(value = "/")
     public String welcomeView(@ModelAttribute User user) {
         return "index";
+    }
+
+    @PostMapping(value = "/addnewperson")
+    public void addNewPerson(@RequestBody String person) {
+        dataSaverService.createPerson(person);
     }
 
 
