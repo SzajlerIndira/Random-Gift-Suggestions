01f25533b755f42d598eb5f346231b11bf577356
Szajler Indira <szajler.indira@gmail.com>
2019-04-03 19:30:24 +0200

    onClick added to html

--- random-gifter/src/main/java/com/codecool/randomgifter/controller/AppRestController.java
+++ random-gifter/src/main/java/com/codecool/randomgifter/controller/AppRestController.java
@@ -4,6 +4,7 @@ import com.codecool.randomgifter.model.Person;
 import com.codecool.randomgifter.service.DataSaverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class AppRestController {
     @Autowired
     DataSaverService dataSaverService;
 
-    @PostMapping("/gifts")
+    @GetMapping("/gifts")
     public String getGiftsByPerson(@RequestBody Person person, Model model) {
         List<String> giftListFromDataSaverService = dataSaverService.getGiftListFromDataSaverService(person.getName());
         String response = "test";
